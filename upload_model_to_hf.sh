e783c787c735d6d1a0768a33f6d831a003d45396
Shengao Wang <wsashawn@bu.edu>
2025-06-12 17:12:18 -0400

    trivial

--- upload_model_to_hf.sh
+++
@@ -1,6 +0,0 @@
-source ~/.bashrc
-conda activate llava
-
-huggingface-cli upload wsashawn/llava_lora_ft_SAYCam_gt0.2_bs640_lr2e-4 ./checkpoints/llava_lora_ft_SAYCam_gt0.2_bs640_lr2e-4
-huggingface-cli upload wsashawn/llava_lora_ft_SAYCam_gt0.2_bs320_lr6e-5 ./checkpoints/llava_lora_ft_SAYCam_gt0.2_bs320_lr6e-5
-huggingface-cli upload wsashawn/llava_lora_ft_SAYCam_gt0.2_bs320_lr6e-4 ./checkpoints/llava_lora_ft_SAYCam_gt0.2_bs320_lr6e-4
